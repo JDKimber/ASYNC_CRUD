d74990941e09453e8f15d076e4d0e95158bafae6
jdkimber <joshua.kimber@wti-solutions.com>
2020-01-19 00:32:31 -0500

    adds async features

--- app.js
+++ app.js
@@ -1,54 +1,28 @@
-const http = new easyHTTP;
+const http = new EasyHTTP;
 
-// Get Posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
+// Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// Get Single Posts
-// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
-
-// Create Data
+// User Data
 const data = {
-  title: 'Custom Post',
-  body: 'This is a custompost'
-};
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
 
-// Create Post
-// http.post('https://jsonplaceholder.typicode.com/posts',
-// data, function(err, post){
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// Update Post
-// http.put('https://jsonplaceholder.typicode.com/posts/1',
-// data, function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+// // Update Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+// Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
